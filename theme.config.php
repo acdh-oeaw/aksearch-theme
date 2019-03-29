013535dd48c3a1bf86ca683a15b660c4073d852f
betullam <betullam@yahoo.de>
2019-03-29 10:00:40 +0100

    Use custom searchbox with permissions.

--- theme.config.php
+++ theme.config.php
@@ -4,5 +4,13 @@ return [
     'less' => [
         'active' => true,
         'compiled.less'
+    ],
+    'helpers' => [
+      'factories' => [
+        'AkSearch\View\Helper\Root\SearchBox' => 'AkSearch\View\Helper\Root\SearchBoxFactory',
+      ],
+      'aliases' => [
+        'searchbox' => 'AkSearch\View\Helper\Root\SearchBox',
       ]
+    ]
 ];
