6c80171f148c3104f4194c75af173677887bfccc
Michael Birkner <michael.birkner@akwien.at>
2019-07-15 13:32:48 +0200

    View helper for bootstrap-datepicker

--- theme.config.php
+++ theme.config.php
@@ -8,9 +8,11 @@ return [
     'helpers' => [
       'factories' => [
         'AkSearch\View\Helper\Root\SearchBox' => 'AkSearch\View\Helper\Root\SearchBoxFactory',
+        'AkSearch\View\Helper\AkSearch\Datepicker' => 'AkSearch\View\Helper\AkSearch\DatepickerFactory',
       ],
       'aliases' => [
         'searchbox' => 'AkSearch\View\Helper\Root\SearchBox',
+        'datepicker' => 'AkSearch\View\Helper\AkSearch\Datepicker',
       ]
     ]
 ];
