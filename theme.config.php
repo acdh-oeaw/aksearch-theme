d6ea8df73c4e8420e3445637d2125f01d5401dab
Michael Birkner <michael.birkner@akwien.at>
2019-08-14 11:54:29 +0200

    Change userdata

--- theme.config.php
+++ theme.config.php
@@ -3,12 +3,15 @@ return [
     'extends' => 'bootstrap3',
     'helpers' => [
       'factories' => [
+        'AkSearch\View\Helper\AkSearch\Datepicker' => 'AkSearch\View\Helper\AkSearch\DatepickerFactory',
         'AkSearch\View\Helper\Root\SearchBox' => 'AkSearch\View\Helper\Root\SearchBoxFactory',
-        'AkSearch\View\Helper\AkSearch\Datepicker' => 'AkSearch\View\Helper\AkSearch\DatepickerFactory'
+        'AkSearch\View\Helper\Root\Auth' => 'VuFind\View\Helper\Root\AuthFactory'
+
       ],
       'aliases' => [
-        'searchbox' => 'AkSearch\View\Helper\Root\SearchBox',
-        'datepicker' => 'AkSearch\View\Helper\AkSearch\Datepicker'
+        'auth' => 'AkSearch\View\Helper\Root\Auth',
+        'datepicker' => 'AkSearch\View\Helper\AkSearch\Datepicker',
+        'searchbox' => 'AkSearch\View\Helper\Root\SearchBox'
       ]
     ],
     'js' => [
