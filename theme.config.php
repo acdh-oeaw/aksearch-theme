1c9f38eb531dd1c667e9593a5121759f1d452274
Michael Birkner <michael.birkner@akwien.at>
2020-12-15 15:39:36 +0100

    Added configurable warning messages in different places

--- theme.config.php
+++ theme.config.php
@@ -4,8 +4,9 @@ return [
     'favicon' => 'aksearch-favicon.ico',
     'helpers' => [
       'factories' => [
-        'AkSearch\View\Helper\Root\AccountMenu' => 'AkSearch\View\Helper\Root\AccountMenuFactory',
         'AkSearch\View\Helper\AkSearch\Datepicker' => 'AkSearch\View\Helper\AkSearch\DatepickerFactory',
+        'AkSearch\View\Helper\AkSearch\Warnings' => 'AkSearch\View\Helper\AkSearch\WarningsFactory',
+        'AkSearch\View\Helper\Root\AccountMenu' => 'AkSearch\View\Helper\Root\AccountMenuFactory',
         'AkSearch\View\Helper\Root\Auth' => 'VuFind\View\Helper\Root\AuthFactory',
         'AkSearch\View\Helper\Root\Citation' => 'VuFind\View\Helper\Root\CitationFactory',
         'AkSearch\View\Helper\Root\Record' => 'VuFind\View\Helper\Root\RecordFactory',
@@ -17,6 +18,7 @@ return [
         'auth' => 'AkSearch\View\Helper\Root\Auth',
         'datepicker' => 'AkSearch\View\Helper\AkSearch\Datepicker',
         'searchbox' => 'AkSearch\View\Helper\Root\SearchBox',
+        'warnings' => 'AkSearch\View\Helper\AkSearch\Warnings',
 
         // Overrides
         'VuFind\View\Helper\Root\Citation' => 'AkSearch\View\Helper\Root\Citation',
