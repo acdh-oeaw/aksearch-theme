39ce9331d687a8d005d9bba4b188766ce92cfc28
Michael Birkner <michael.birkner@akwien.at>
2020-12-16 06:46:47 +0100

    Fixed typo

--- js/check_item_statuses.js
+++ js/check_item_statuses.js
@@ -1,4 +1,4 @@
-/* AK: Overriding original check_itesm_statuses.js in bootstrap theme */
+/* AK: Overriding original check_item_statuses.js in bootstrap theme */
 VuFind.register('itemStatuses', function ItemStatuses() {
   // AK: We don't (yet) use item statuses
   return false;
