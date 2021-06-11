c9d117914bb9fbfa7471f2f076c5f5c27e4b0016
Michael Birkner <michael.birkner@akwien.at>
2021-06-11 14:33:38 +0200

    Better mobile theme and "X" button in searchbox for clearing it

---
+++ js/clearSearchbox.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Clear searchbox when clicking the "X"
+    $('#searchForm_lookfor_clear').click(function() {
+        $('#searchForm_lookfor').val('');
+    });
+});
